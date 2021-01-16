e91b5ddbf06f5fac64539c52ca12675c7cffe414
Mehrshad <m-b850@protonmail.com>
2021-01-16 18:57:03 +0330

    added author User debug return str

--- staff/models.py
+++ staff/models.py
@@ -1,27 +1,29 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title  = models.CharField(max_length=100)
-    text =  models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
     crated_date = models.DateTimeField(default=timezone.now)
     post_is_ready = 'R'
-    post_isnt_ready = 'NR'
+    post_not_ready = 'NR'
     STATUS = [
         (post_is_ready, 'Ready'),
-        (post_isnt_ready,'Not Ready'),
+        (post_not_ready, 'Not Ready'),
     ]
     status = models.CharField(
         max_length=2,
         choices=STATUS,
-        default=post_isnt_ready,
+        default=post_not_ready,
         )
 
     def publish(self):
         self.status = "R"
         self.save()
+
     def __str__(self):
-        self.title
+        return self.title

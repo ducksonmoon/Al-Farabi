f93200921115bf3ae5d8847189cc1ebb51cb4a3f
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-12-31 12:11:52 +0330

    Change in Staff model

--- staff/models.py
+++ staff/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
-# Create your models here.
+
+"""
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
@@ -26,3 +27,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+"""

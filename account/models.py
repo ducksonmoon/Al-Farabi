a46faf50fa6302170933021445dca7b108a1afde
Mehrshad <MehrshadBaqerzadegan@gmail.com>
2021-12-31 12:22:09 +0330

    Add university model

--- account/models.py
+++ account/models.py
@@ -2,14 +2,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Position(models.Model):
-	name = models.CharField(max_length=50)
+class University(models.Model):
+	name = models.CharField(max_length=225)
+	symbol_name = models.CharField(max_length=225)
+	logo = models.ImageField(upload_to='university_logo')
 
 
 class Team(models.Model):
+	name = models.CharField(max_length=50)
+	university = models.ForeignKey('University', on_delete=models.CASCADE)
+
+
+class Position(models.Model):
 	name = models.CharField(max_length=50)
 
 
 class Account(AbstractUser):
 	team = models.ForeignKey(Team, null=True, on_delete=models.CASCADE)
 	position = models.ForeignKey(Position, null=True, on_delete=models.CASCADE)
+	university = models.ForeignKey(University, null=True, on_delete=models.CASCADE)
